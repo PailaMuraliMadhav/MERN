556a2bc51b2b512888bfae05d26a8606c238b88a
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-16 14:26:24 +0530

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello !")
+
+//variables->
+// --------------------------------------
+//  let- 
+let Name = "Murali paila" 
+// let a=20;
+// a=10; (error)
+// const
+const city="Vizag";
+//  var(we can re assign the number)
+var age =18;
+age=20;
+var age=30;
+console.log(Name)
+console.log(city)
+console.log(age)
+// sum of two numbers
+var a=20;
+var b=30;
+var c=a+b;
+console.log("Sum:",c);
+// ---------------------------------------
+// datatypes->
+// primitive- 
+// String  = 
+var a= 7;
+let n="Murali";
+var t =true;
+
+
+console.log(typeof a);
+console.log(typeof n);
+console.log(typeof t);
+
+// Number
+// boolean
+// BigInt
+// syntax
+// Nonprimitive-
+// Function
+// Array
+// Object
