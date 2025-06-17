4d736b9f71c23abd84e962e17592ee3b9449cbf6
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-17 09:53:38 +0530

    commit

--- Day2/index.js
+++ Day2/index.js
@@ -1,3 +1,5 @@
+// CONTROL STATEMENTS
+
 let marks=90;
 if(marks >85){
     console.log("A+");
@@ -10,7 +12,7 @@ if(we == "Sunny"){
 else{
     console.log("stay at home!");
 }
-console.log("-------------------------                       ")
+
 // ----------------------------------------
 let traffic="green";
 if(traffic == "red"){
@@ -18,6 +20,68 @@ if(traffic == "red"){
 }else if(traffic == "yellow"){
     console.log("On your marks");
 }
-else{
+else if(traffic == "green"){
     console.log("Go");
+}
+else{
+    console.log("Error !");
+}
+// ----------------------------------------------
+
+let m=50;
+if(m>=90){
+    console.log("A")
+}
+else if(m >= 75){
+    console.log("B")
+}
+else if(m > 40){
+    console.log("C")
+}
+else if(m==40){
+    console.log("Pass")
+}
+else{
+    console.log("Fail")
+}
+// ------------------------------------------------------
+
+//  SWITCH STATEMENT
+
+let option= 6;
+
+switch(option){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3: 
+        console.log("Wedneasday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid!");
+
+}
+
+// ----------------------------------------------------
+
+// LOOPS
+
+// 1.FOR LOOP
+for(let i=1;i<=7;i++){
+    console.log(i);
 }
