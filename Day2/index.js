7d8e050afeb636e50233d8a05659a77603e8211f
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-17 09:31:08 +0530

    commit

---
+++ Day2/index.js
@@ -0,0 +1,23 @@
+let marks=90;
+if(marks >85){
+    console.log("A+");
+}
+// ---------------------------------------
+let we="rainy";
+if(we == "Sunny"){
+    console.log("Go to beach");
+}
+else{
+    console.log("stay at home!");
+}
+console.log("-------------------------                       ")
+// ----------------------------------------
+let traffic="green";
+if(traffic == "red"){
+    console.log("STOP");
+}else if(traffic == "yellow"){
+    console.log("On your marks");
+}
+else{
+    console.log("Go");
+}
