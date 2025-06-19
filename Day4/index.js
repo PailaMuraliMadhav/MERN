906e989b7151dc4a1663e27d1b4017eb55c79af1
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-19 11:38:31 +0530

    Add additional console logs for asynchronous behavior demonstration

--- Day4/index.js
+++ Day4/index.js
@@ -40,3 +40,18 @@ console.log("AMn");
 setTimeout(() => console.log("HI"),0);
 console.log(1);
 console.log("Okkay");
+
+
+
+console.log("start");
+setTimeout(() => {
+    console.log("Timeout");
+},0);
+
+Promise.resolve().then(() =>
+   console.log("Promise")
+);
+console.log("End");
+
+
+
