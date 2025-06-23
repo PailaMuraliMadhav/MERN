d6f2023824548f13ed3944f26f243ecdc9b24349
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-06-23 10:46:28 +0530

    Add initial implementation of file operations and package configuration

---
+++ Day6/index.js
@@ -0,0 +1,45 @@
+// Node js  - is the javascript run time environment.
+// run time environment is the environment in which the code is executed.
+// module vs package - in module we run by node.js and package is done by npm.
+// npm - is the package manager.
+
+// //! create a file 
+const fs = require('node:fs');
+/*
+fs.writeFile("Hello.txt","How are you ?????",function(err){
+    if(err) console.log("Error ",err)
+        else( console.log("File created !"))
+})  //callback is a function that is called after the execution of the code.
+*/
+// ! Append a file
+/*
+fs.appendFile("Hello.txt", "\n This is changed",function(err){
+    if(err) console.log("Error",err)
+        else (console.log("File appended!"))
+})
+*/
+// Rename  a  file 
+/*
+fs.rename("Hello.txt","Hello2.txt",function(err){
+    if(err) console.log("error",err)
+        else(console.log("File renamed!"))
+})*/
+// Write a file -> this completely erase the previious written and freshly starts
+/*
+fs.writeFile("Hello2.txt","Hi Murali ",function(err){
+    if(err)console.log("Error",err)
+        else(console.log("File written"))
+})
+
+fs.appendFile("Hello2.txt","\nHow are you ?? ",function(err){
+    if(err) console.log("Error",err)
+        else(console.log("Appended!"))
+})
+*/
+// Deleted a file
+/*
+fs.unlink("Hello2.txt",function(err){
+    if(err) console.log("Error",err)
+        else(console.log("File deleted"))
+})
+*/
